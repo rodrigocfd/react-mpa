8a66ad2cc10091fabd38313faa9af12ed2d76b6e
Rodrigo Cesar de Freitas Dias <rcesar@gmail.com>
2020-05-08 16:17:54 -0300

    Improving HTML template.

--- src/Index.page.js
+++ src/Index.page.js
@@ -4,14 +4,14 @@ import ReactDOM from 'react-dom';
 import Header from 'src/common/Header';
 import frog from './frog.jpg';
 
-ReactDOM.render(<Index />,
-	document.getElementById('mountPoint'));
-
 function Index() {
 	return (<>
 		<Header />
 		<h1>This is main index.</h1>
-		<p><a href="second/second.html">Second</a></p>
+		<p><a href="second/second.html">Go to Second</a></p>
 		<p><img src={frog} /></p>
 	</>);
 }
+
+ReactDOM.render(<Index />,
+	document.getElementById('mountPoint'));
--- src/second/Second.page.js
+++ src/second/Second.page.js
@@ -6,15 +6,15 @@ import TitleFool from './TitleFool';
 import apple from './apple.jpg';
 import c from './second.css';
 
-ReactDOM.render(<Second />,
-	document.getElementById('mountPoint'));
-
 function Second() {
 	return (<>
 		<Header />
 		<TitleFool name="You" />
 		<p className={c.red}>You came to the second.</p>
-		<p><a href="../index.html">Index</a></p>
+		<p><a href="../index.html">Go to Index</a></p>
 		<p><img src={apple} /></p>
 	</>);
 }
+
+ReactDOM.render(<Second />,
+	document.getElementById('mountPoint'));
