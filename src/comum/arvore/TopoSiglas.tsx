e88a644ca4de671ba893c7f8ae9982c6e308cd7b
Rodrigo Cesar de Freitas Dias <rcesar@gmail.com>
2020-05-21 17:39:07 -0300

    TopoSiglas: RepúblicaFdB pode não ser o raiz.

--- src/comum/arvore/TopoSiglas.tsx
+++ src/comum/arvore/TopoSiglas.tsx
@@ -9,20 +9,26 @@ interface Props {
 
 function TopoSiglas({tripaUnids}: Props) {
 	let trilha = '';
-	const maxN = 6; // quantidade máxima de níveis hierárquicos a serem exibidos
 
-	if (tripaUnids.length > maxN + 1) { // mais unidades que o máximo, algumas não serão exibidas
-		trilha += '... › '
-	}
+	if (tripaUnids.length > 0) {
+		const maxN = 6; // quantidade máxima de níveis hierárquicos a serem exibidos
 
-	const inicio = Math.max(1, tripaUnids.length - maxN); // pula "República Federativa do Brasil", que não tem sigla
-	for (let i = inicio; i < tripaUnids.length; ++i) {
-		trilha += tripaUnids[i].sigla + ' › ';
-	}
-	trilha = trilha.substr(0, trilha.length - 3);
+		if (tripaUnids.length > maxN + 1) { // mais unidades que o máximo, algumas não serão exibidas
+			trilha += '... › '
+		}
 
-	if (!trilha.length) { // vazio?
-		trilha = '\u00A0'; // &nbsp; pra não colapsar verticalmente a div
+		const uBase = (tripaUnids[0].id === 1) ? 1 : 0; // República Federativa do Brasil não tem sigla, pula
+		const inicio = Math.max(uBase, tripaUnids.length - maxN);
+		for (let i = inicio; i < tripaUnids.length; ++i) {
+			let sigla = tripaUnids[i].sigla;
+			if (sigla.length === 0 || sigla === ' ') sigla = '∅';
+			trilha += sigla + ' › ';
+		}
+		trilha = trilha.substr(0, trilha.length - 3);
+
+		if (!trilha.length) { // vazio?
+			trilha = '\u00A0'; // &nbsp; pra não colapsar verticalmente a div
+		}
 	}
 
 	return (
