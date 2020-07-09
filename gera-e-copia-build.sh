4c1d21402bd49e2e484c093548b1d02725c17ede
Rodrigo Cesar de Freitas Dias <rcesar@gmail.com>
2020-07-09 11:17:40 -0300

    Melhorando LEIAME e script de build.

--- gera-e-copia-build.sh
+++ gera-e-copia-build.sh
@@ -1,18 +1,42 @@
 ##
-## Gera a build via NPM e copia para dentro do workspace do Siorg
+## Gera a build via NPM e copia para dentro do workspace do Siorg.
+## A flag "-d" simplesmente deleta a build do workspace, sem gerar uma nova.
 ##
 
-# Altere este caminho para o diretório na sua máquina!
-FINAL_DEST=~/siorg/workspace/novosiorg/siorg-gestao/siorg-gestao-webapp/src/main/webapp/public
+# Altere este caminho para o seu diretório workspace do Siorg.
+WORKSPACE_DIR=~/siorg/workspace
 
-# Este diretório está amarrado dentro da configuração do Webpack, não mexa.
+# Destino da build.
+FINAL_DEST="$WORKSPACE_DIR/novosiorg/siorg-gestao/siorg-gestao-webapp/src/main/webapp/public"
+
+# O nome do diretório da build deve ser o mesmo que está em webpack.config.js:
+# module.exports -> output -> path
 BUILD_DIR=build
 
-# Checa se o diretório do workspace Siorg está certo.
+# Checa se o diretório do workspace Siorg existe.
 if [ ! -d "$FINAL_DEST" ]; then
-	echo "-- O diretório do workspace do Siorg está errado, corrija-o neste script."
+	echo "-- O diretório do workspace do Siorg não existe:"
+	echo $WORKSPACE_DIR
+	echo "-- Build abortada."
 	exit 0
 fi
+
+# Verifica argumentos passados ao script.
+while [[ "$#" -gt 0 ]]; do
+	case $1 in
+		# Se foi passado o argumento "-d", deleta a build do workspace e encerra o script.
+		-d)
+		if [ -d "$FINAL_DEST/$BUILD_DIR" ]; then
+			echo "-- Removendo diretório de build existente dentro do workspace do Siorg..."
+			rm -Rf "$FINAL_DEST/$BUILD_DIR"
+			echo "Script encerrado."
+		else
+			echo "-- Não há build dentro do workspace. Script encerrado."
+		fi
+		exit 0
+		;;
+	esac
+done
 
 # Checa se estamos no diretório raiz da aplicação.
 if [ ! -d ./src ] || [ ! -f ./package.json ]; then
@@ -20,21 +44,23 @@ if [ ! -d ./src ] || [ ! -f ./package.json ]; then
 	exit 0
 fi
 
+# Gera a build com os arquivos HTML, JS e CSS.
 echo "-- Gerando build via NPM..."
 npm run build
 
-# O diretório da build foi gerado?
+# O diretório da build foi realmente gerado?
 if [ ! -d "./$BUILD_DIR" ]; then
 	echo "-- A geração da build via NPM falhou."
 	exit 0
 fi
 
-# Se o diretório existe, deleta-o com tudo dentro.
+# Se existe uma build gerada dentro do workspace, deleta com tudo dentro.
 if [ -d "$FINAL_DEST/$BUILD_DIR" ]; then
 	echo "-- Removendo diretório de build existente dentro do workspace do Siorg..."
 	rm -Rf "$FINAL_DEST/$BUILD_DIR"
 fi
 
+# Copia a build para dentro do workspace.
 echo "-- Copiando diretório de build:"
 echo "-- $FINAL_DEST"
 cp -r "./$BUILD_DIR" "$FINAL_DEST/."
